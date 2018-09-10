de6065ec20be396a621a7501df5c1cbe4cbf904b
ningmengjean <ningmengjean@gmail.com>
2018-09-10 11:41:04 +0800

    first commit

---
+++ PhotoShow/PhotoCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  PhotoCollectionViewCell.swift
+//  PhotoShow
+//
+//  Created by wangchi on 2018/9/6.
+//  Copyright © 2018年 Zhu xiaojin. All rights reserved.
+//
+
+import UIKit
+
+class PhotoCollectionViewCell: UICollectionViewCell {
+    
+}
